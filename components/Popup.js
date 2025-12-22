3334bbf78f5f8214d12bb1e0cff0d75861645326
jgyb89 <jgreen.gus@gmail.com>
2025-12-22 11:10:42 -0600

    Updated Popup class, Esc event listener, and open/close public methods

--- components/Popup.js
+++ components/Popup.js
@@ -1,15 +1,38 @@
-// Make it a child class of Popup
 class Popup {
   constructor({ popupSelector }) {
     this._popupElement = document.querySelector(popupSelector);
-    console.log(this._popupElement);
+    this._handleEscClose = this._handleEscClose.bind(this);
   }
 
   open() {
     this._popupElement.classList.add("popup_visible");
+    // Add Esc listener when opened
+    document.addEventListener("keydown", this._handleEscClose);
   }
 
-  close() {}
+  close() {
+    this._popupElement.classList.remove("popup_visible");
+    // Remove Esc listener when closed
+    document.removeEventListener("keydown", this._handleEscClose);
+  }
+
+  _handleEscClose(evt) {
+    if (evt.key === "Escape") {
+      this.close();
+    }
+  }
+
+  setEventListeners() {
+    this._popupElement.addEventListener("mousedown", (evt) => {
+      // Close if clicking overlay or close button
+      if (
+        evt.target.classList.contains("popup_visible") ||
+        evt.target.classList.contains("popup__close")
+      ) {
+        this.close();
+      }
+    });
+  }
 }
 
 export default Popup;
